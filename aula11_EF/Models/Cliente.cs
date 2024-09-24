4bb269dc7d9d541e5686fcb92479c5e94006a461
LuisfilipesUnipam <luisfilipes@unipam.edu.br>
2024-09-24 23:26:40 +0000

    Aula EntityFramework

---
+++ aula11_EF/Models/Cliente.cs
@@ -0,0 +1,17 @@
+namespace aula11_EF.Models{
+
+    public class Cliente{
+        public int Id { get; set;}
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public Cliente(string nome, int idade){
+            Nome = nome;
+            Idade = idade;
+        }
+
+        public override string ToString(){
+            return $"Nome: {Nome}, Idade: {Idade}";
+        }
+    }
+}
